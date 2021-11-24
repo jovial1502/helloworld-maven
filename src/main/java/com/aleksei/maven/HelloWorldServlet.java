afe20ea699869a0a0a84d35765a38dd774383b0a
jovial1502 <jovial1502@gmail.com>
2021-11-24 11:31:51 +0100

    Normal stand

--- src/main/java/com/aleksei/maven/HelloWorldServlet.java
+++ src/main/java/com/aleksei/maven/HelloWorldServlet.java
@@ -18,7 +18,7 @@ public class HelloWorldServlet extends HttpServlet {
         resp.setContentType("text/html");
 
         PrintWriter out = resp.getWriter();
-        out.print("Hello World from Git!");
+        out.print("Hello World from Git from our presentation!");
         out.flush();
         out.close();
     }
